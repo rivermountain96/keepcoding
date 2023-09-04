6be893475dc1d355b7b3bf56030b2186d90217e5
sunchoi22 <sunchoi22@pos.idserve.net>
2023-09-04 22:10:37 +0900

    [ADD]members php 추가

---
+++ admin/members/tmember_list.php
@@ -0,0 +1,179 @@
+<?php
+  include_once $_SERVER['DOCUMENT_ROOT'].'/keepcoding/admin/inc/header.php';
+?>
+
+<!-- 최성희 tmember_list 시작 -->
+<div class="content">
+  <h2 class="pd72 fs-4">강사 관리</h2>
+  <div class="btn-group pd48" role="group" aria-label="Default button group">
+    <button type="button" class="btn btn-outline-primary">접속중</button>
+    <button type="button" class="btn btn-outline-primary">미접속</button>
+    <button type="button" class="btn btn-outline-primary active">전체</button>
+  </div>
+  <table class="table tmember_list_table">
+    <thead>
+      <tr class="fw-bold">
+        <th class="col-1">No.</th>
+        <th class="col-2">ID</th>
+        <th class="col-2">이름</th>
+        <th class="col-2">상태</th>
+        <th class="col-2">최근 접속일</th>
+        <th class="col-2">가입일</th>
+        <th class="col-1">삭제</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- tmember_list_tr 시작 -->
+      <tr>
+        <td class="align-middle">10</td>
+        <td class="align-middle"><a href="#">penny21</a></td>
+        <td class="align-middle">이민준</td>
+        <td class="align-middle">접속중</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">
+          <button type="button" class="btn btn-outline-primary btn-sm">삭제</button>
+        </td>
+      </tr>
+      <!-- tmember_list_tr 끝 -->
+      <!-- tmember_list_tr 시작 -->
+      <tr>
+        <td class="align-middle">9</td>
+        <td class="align-middle"><a href="#">riley</a></td>
+        <td class="align-middle">김서준</td>
+        <td class="align-middle">접속중</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">
+          <button type="button" class="btn btn-outline-primary btn-sm">삭제</button>
+        </td>
+      </tr>
+      <!-- tmember_list_tr 끝 -->
+      <!-- tmember_list_tr 시작 -->
+      <tr>
+        <td class="align-middle">8</td>
+        <td class="align-middle"><a href="#">ringo15</a></td>
+        <td class="align-middle">강도현</td>
+        <td class="align-middle">접속중</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">
+          <button type="button" class="btn btn-outline-primary btn-sm">삭제</button>
+        </td>
+      </tr>
+      <!-- tmember_list_tr 끝 -->
+      <!-- tmember_list_tr 시작 -->
+      <tr>
+        <td class="align-middle">7</td>
+        <td class="align-middle"><a href="#">trisha</a></td>
+        <td class="align-middle">김지아</td>
+        <td class="align-middle">접속중</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">
+          <button type="button" class="btn btn-outline-primary btn-sm">삭제</button>
+        </td>
+      </tr>
+      <!-- tmember_list_tr 끝 -->
+      <!-- tmember_list_tr 시작 -->
+      <tr>
+        <td class="align-middle">6</td>
+        <td class="align-middle"><a href="#">robin</a></td>
+        <td class="align-middle">박우진</td>
+        <td class="align-middle">접속중</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">
+          <button type="button" class="btn btn-outline-primary btn-sm">삭제</button>
+        </td>
+      </tr>
+      <!-- tmember_list_tr 끝 -->
+      <!-- tmember_list_tr 시작 -->
+      <tr>
+        <td class="align-middle">5</td>
+        <td class="align-middle"><a href="#">tylor</a></td>
+        <td class="align-middle">신선우</td>
+        <td class="align-middle">미접속</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">
+          <button type="button" class="btn btn-outline-primary btn-sm">삭제</button>
+        </td>
+      </tr>
+      <!-- tmember_list_tr 끝 -->
+      <!-- tmember_list_tr 시작 -->
+      <tr>
+        <td class="align-middle">4</td>
+        <td class="align-middle"><a href="#">shelby</a></td>
+        <td class="align-middle">유서진</td>
+        <td class="align-middle">접속중</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">
+          <button type="button" class="btn btn-outline-primary btn-sm">삭제</button>
+        </td>
+      </tr>
+      <!-- tmember_list_tr 끝 -->
+      <!-- tmember_list_tr 시작 -->
+      <tr>
+        <td class="align-middle">3</td>
+        <td class="align-middle"><a href="#">umberto</a></td>
+        <td class="align-middle">이유준</td>
+        <td class="align-middle">미접속</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">
+          <button type="button" class="btn btn-outline-primary btn-sm">삭제</button>
+        </td>
+      </tr>
+      <!-- tmember_list_tr 끝 -->
+      <!-- tmember_list_tr 시작 -->
+      <tr>
+        <td class="align-middle">2</td>
+        <td class="align-middle"><a href="#">sierra</a></td>
+        <td class="align-middle">이민아</td>
+        <td class="align-middle">미접속</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">
+          <button type="button" class="btn btn-outline-primary btn-sm">삭제</button>
+        </td>
+      </tr>
+      <!-- tmember_list_tr 끝 -->
+      <!-- tmember_list_tr 시작 -->
+      <tr>
+        <td class="align-middle">1</td>
+        <td class="align-middle"><a href="#">solomon</a></td>
+        <td class="align-middle">이현우</td>
+        <td class="align-middle">미접속</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">2023.09.09</td>
+        <td class="align-middle">
+          <button type="button" class="btn btn-outline-primary btn-sm">삭제</button>
+        </td>
+      </tr>
+      <!-- tmember_list_tr 끝 -->
+      
+    </tbody>
+  </table>
+  <div class="d-flex align-items-center pd48">
+    <nav aria-label="Page navigation example" class="col-11">
+      <ul class="pagination justify-content-center">
+        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <li class="page-item active"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">4</a></li>
+        <li class="page-item"><a class="page-link" href="#">5</a></li>
+        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+      </ul>
+    </nav>
+    <button type="button" class="btn btn-primary col-1">회원등록</button>
+  </div>
+  
+</div>
+<!-- 최성희 tmember_list 끝 -->
+
+<?php
+  include_once $_SERVER['DOCUMENT_ROOT'].'/keepcoding/admin/inc/footer.php';
+?>
