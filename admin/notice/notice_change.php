fb44d14a46e74c6f77e68b225024e4dfa00e63e9
jeongewon <137126410+jeongewon@users.noreply.github.com>
2023-09-05 19:30:24 +0900

    [FIX] notice.php 오류 수정

--- admin/notice/notice_change.php
+++ admin/notice/notice_change.php
@@ -6,47 +6,44 @@
 <!-- 최성희 notice_change 시작 -->
 <div class="content notice_change">
   <h2 class="pd72 fs-4">공지 수정</h2>
-    <form class="pd48" action="#.php" id="notice_change_submit">
+    <form class="pd48" action="/keepcoding/admin/notice/notice_change_ok.php" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="idx" id="idx" value="<?= $bno ?>">
       <div class="pd24">
-        <label class="pd10 h6" for="notice_title">제목</label>
-        <input class="form-control form-control-lg" id="notice_title" type="text" value="에디터가 만들어가는 헬로 킵콘 이야기" aria-label="default input example">
+        <label class="pd10 h6" for="title">제목</label>
+        <input class="form-control form-control-lg" id="title" name="title" type="text"  aria-label="default input example" value="<?= $rs -> title; ?>">
       </div>
       
-      <div class="row pd24 gap-3">
-        <div class="col p-0">
+      <div class="row pd24 gap-3 form-width-973">
+        <div class="col-4 p-0">
           <p class="pd10 h6">공지 기한</p>
           <select class="form-select form-select-lg" aria-label="Small select example">
           <option value="1" selected>제한</option>
           <option value="2">무제한</option>
           </select>
         </div>
-        <div class="notice_change_regdate col p-0 datepicker">
-          <label class="pd10 h6" for="datepicker_start">시작일</label>
-          <input type="text" id="datepicker_start" name="datepicker" class="form-control form-control-lg" value="2023.09.09">
+        <div class="notice_change_regdate col-4 p-0 datepicker">
+          <label class="pd10 h6" for="datepicker">시작일</label>
+          <input type="text" id="datepicker" name="regdate" class="form-control form-control-lg" value="<?= $rs -> regdate; ?>">
         </div> 
-        <div class="notice_change_enddate col p-0 datepicker">
-          <label class="pd10 h6" for="datepicker_end">만료일</label>
-          <input type="text" id="datepicker_end" name="datepicker" class="form-control form-control-lg" value="2023.09.09">
-        </div>
+        <!-- <div class="notice_change_enddate col p-0 datepicker">
+          <label class="pd10 h6" for="duedate">만료일</label>
+          <input type="text" id="datepicker2" name="duedate" class="form-control form-control-lg" value="<?= $rs -> duedate; ?>">
+        </div> -->
       </div>
 
       <div class="pd24">
         <p class="pd10 h6">내용</p>
-        <div id="summernote">
-          <br>
-          <h1>1. 에디터가 만들어가는 헬로 킵콘 이야기 🙆‍♀️</h1>  
-          <br> 이번 호에서는 헬로 킵콘이 지금까지 달려온 1년간의 시간을 돌아보며, 헬로 킵콘을 만드는 5명의 에디터의 이야기를 담아봤어요.
-        </div>
+        <textarea id="summernote" name="content"><?= $rs -> content; ?></textarea>
       </div>
 
       <div class="pd48">
-        <label for="formFile" class="form-label pd10 h6">이미지</label>
-        <input class="form-control form-control-lg form-control form-control-lg" id="formFile" type="file">
+        <label for="notice_img" class="form-label pd10 h6">이미지</label>
+        <input class="form-control form-control-lg form-control form-control-lg" id="notice_img" name="notice_img" value="<?= $rs -> notice_img; ?>" type="file">
       </div>
 
       <div class="d-flex justify-content-end gap-3">
-        <button type="button" class="btn btn-primary" form="notice_change_submit">수정</button>
-        <button type="button" class="btn btn-secondary" onClick="location.href='notice_list.php'">취소</button>
+        <button type="submit" class="btn btn-primary">수정</button>
+        <button type="button" class="btn btn-secondary" onclick="history.back(1)">취소</button>
       </div>
     </form>
 </div>
--- admin/notice/notice_view.php
+++ admin/notice/notice_view.php
@@ -6,33 +6,41 @@
 <!-- 최성희 notice_view 시작 -->
 <div class="content notice_view">
   <h2 class="pd48 fs-4">공지 상세보기</h2>
+
   <div class="pd24">
   <hr class="pd24">
+
+  <input type="hidden" name="idx" value="<?php echo $bno;?>">
     <div class="pd24">
       <h3 class="pd24 h6 text-secondary">제목</h3>
         <div class="d-flex row">
-          <p class="h6 col-5 p-0">에디터가 만들어가는 헬로 킵콘 이야기</p>
-          <p class="col-5">2023.09.09 ~ 2024.09.09</p>
+          <p class="h6 col-5 p-0"><?= $rs -> title; ?></p>
+          <p class="col-5">
+          <?php $date = str_replace('-', '.', $rs -> regdate); echo $date?>
+            <!-- <?php $date = str_replace('-', '.', $rs -> regdate); echo $date?> ~ <?php $date = str_replace('-', '.', $rs -> duedate); echo $date?> -->
+          </p>
         </div>
     </div>
   <hr class="pd24">
     <div class="pd24">
       <h3 class="pd24 h6 text-secondary">이미지</h3>
-      <img src="/keepcoding/admin/img/notice_view_smile.png" alt="">
+      <img src="<?= $rs -> notice_img; ?>" alt="">
     </div>
   <hr class="pd24">
     <div class="pd24">
       <h3 class="pd24 h6 text-secondary">내용</h3>
-      <h1>1. 에디터가 만들어가는 헬로 킵콘 이야기 🙆‍♀️</h1>  
-      <br> 이번 호에서는 헬로 킵콘이 지금까지 달려온 1년간의 시간을 돌아보며, 헬로 킵콘을 만드는 5명의 에디터의 이야기를 담아봤어요.
+      <p><?= $rs -> content; ?></p>  
     </div>
+
   <hr class="pd48">
       <div class="d-flex justify-content-end gap-3">
-        <button type="button" class="btn btn-primary" onClick="location.href='notice_change.php'">수정</button>
-        <button type="button" class="btn btn-secondary">삭제</button>
-        <button type="button" class="btn btn-primary" onClick="location.href='notice_list.php'">공지 리스트</button>
+        <a href="notice_change.php?idx=<?= $bno; ?>" class="btn btn-primary">수정</a>
+        <a href="notice_del.php?idx=<?= $bno; ?>" id="delete" class="btn btn-secondary">삭제</a>
+        <a href="notice_list.php" class="btn btn-primary">공지 리스트</a>
       </div>
+
     </div>
+
 </div>
   <!-- 최성희 notice_view 끝 -->
   
