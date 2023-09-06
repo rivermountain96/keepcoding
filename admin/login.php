a3f720526db61cff30c799ea5afd003dfc27829d
jeongewon <137126410+jeongewon@users.noreply.github.com>
2023-09-06 20:57:50 +0900

    [FIX] main.js 수정, login.php js 수정

--- admin/login.php
+++ admin/login.php
@@ -119,7 +119,7 @@
   </div>
 </dialog>
 <!-- 이강산 DIALOG POPUP 끝 -->
-<!-- 
+
 <script>
 $(document).ready(function(){
   var key = getCookie('admin'); 
@@ -145,36 +145,7 @@ $(document).ready(function(){
     }
   });
 });
-
-function setCookie(cookieName, value, exdays){
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var cookieValue = escape(value) + ((exdays==null) ? "" : "; expires=" + exdate.toGMTString());
-    document.cookie = cookieName + "=" + cookieValue;
-}
- 
-// 쿠키삭제
-function deleteCookie(cookieName){
-  var expireDate = new Date();
-  expireDate.setDate(expireDate.getDate() - 1);
-  document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
-}
-
-// 쿠키 가져오기
-function getCookie(cookieName) {
-  cookieName = cookieName + '=';
-  var cookieData = document.cookie;
-  var start = cookieData.indexOf(cookieName);
-  var cookieValue = '';
-  if(start != -1){
-    start += cookieName.length;
-    var end = cookieData.indexOf(';', start);
-    if(end == -1)end = cookieData.length;
-    cookieValue = cookieData.substring(start, end);
-  }
-  return unescape(cookieValue);
-}
-</script> -->
+</script>
 
 
 <?php
