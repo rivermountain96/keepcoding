79f75fa92327399a0aa1d8fa7ef4b12bed22ef39
sunchoi22 <sunchoi22@pos.idserve.net>
2023-09-06 09:57:04 +0900

    [FIX] 뷰카테 오류 코드 추가
    이원님 공유해주신 코드 추가

--- admin/product/view_cate.php
+++ admin/product/view_cate.php
@@ -11,27 +11,29 @@
   }
 
   $result = $mysqli -> query($query); //쿼리실행결과를 $result 할당
+
+  $html = '';
   
-  while($rs = $result -> fetch_object()){
-    $rsc[] = $rs;
+  if(isset($result)){
+    while($rs = $result -> fetch_object()){
+      $rsc[] = $rs;
+    }
+    if(isset($rsc)){
+      foreach($rsc as $ct){
+
+        $html .= "<li class=\"list-group-item big d-flex justify-content-between align-items-center\" data-cid=\"".$ct -> cid."\" data-step=\"".$ct -> step."\">
+            <span class=\"cate_size\">".$ct -> name."</span>
+            <div class=\"cate_edit_btns d-flex gap-3\">
+                <a href=\"\" class=\"edit_btn\" data-bs-toggle=\"modal\" data-bs-target=\"#cateEditModal\">수정</a>
+                <a href=\"\" class=\"delete_btn\">삭제</a>
+            </div>
+        </li>";
+      }
+    }
+    
+  echo $html;
+  }else{
+    echo $html;
   }
 
-$html = '';
-
-foreach($rsc as $ct){
-
-  $html .= "<li class=\"list-group-item big d-flex justify-content-between align-items-center\" data-cid=\"".$ct -> cid."\" data-step=\"".$ct -> step."\">
-      <span class=\"cate_size\">".$ct -> name."</span>
-      <div class=\"cate_edit_btns d-flex gap-3\">
-          <a href=\"\" class=\"edit_btn\" data-bs-toggle=\"modal\" data-bs-target=\"#cateEditModal\">수정</a>
-          <a href=\"\" class=\"delete_btn\">삭제</a>
-      </div>
-  </li>";
-}
-
-
-
-
-
-  echo $html;
 ?>
