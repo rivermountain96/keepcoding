ec87f290c7606e72e86696751649f93cd3007ca2
rivermountain96 <dlrkdtks7@gmail.com>
2023-09-07 11:43:55 +0900

    [FIX] login.php의 popup 수정

--- admin/login.php
+++ admin/login.php
@@ -100,10 +100,10 @@
       <dd><span>최*희</span> : 강좌관리/쿠폰등록/공지사항/QNA/강사&수강생 관리</dd>
       <dt><span>- 백엔드 구현 -</span></dt>
       <dd><span>박*용</span> : 강좌리스트</dd>
-      <dd><span>이*산</span> : 강좌관리/강좌등록</dd>
+      <dd><span>이*산</span> : 강좌관리/강좌등록/QNA</dd>
       <dt><span>- 퍼블리싱/백엔드 구현 -</span></dt>
       <dd><span>정*원</span> : 로그인/쿠폰등록/공지사항</dd>
-      <dd><span>이*서</span> : 헤더/대시보드/카테고리관리/쿠폰관리/</dd>
+      <dd><span>이*서</span> : 헤더/대시보드/카테고리관리/쿠폰관리</dd>
     </dl>
   </div>
 
