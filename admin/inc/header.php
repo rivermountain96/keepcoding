f90034e995e4098d25d4d017155c4345144f17f4
triallife <nanamutok@gmail.com>
2023-09-07 13:56:00 +0900

    [FIX] html validator 반영 (favicon)

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -8,10 +8,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-   <!-- favicon -->
-   <link rel="icon" type="image/png" sizes="16x16" href="/keepcoding/admin/img/keepcoding_favicon/logo_favicon.ico">
+  <!-- favicon -->
+  <link rel="icon" type="image/png" sizes="16x16" href="/keepcoding/admin/img/keepcoding_favicon/logo_favicon.ico">
   <link rel="manifest" href="/keepcoding/admin/img/keepcoding_favicon/manifest.json">
-  <meta name="msapplication-TileColor" >
 
   <!-- style -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
