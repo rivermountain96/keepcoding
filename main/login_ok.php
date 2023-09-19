11b025a318e5dfd89cc88c0ddb3a666665591786
rivermountain96 <dlrkdtks7@gmail.com>
2023-09-19 16:18:52 +0900

    [ADD] login_ok, logout.php 추가

---
+++ main/login_ok.php
@@ -0,0 +1,40 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/keepcoding/main/inc/dbcon.php';
+
+if(isset($_SESSION['UID'])){
+
+    echo "<script>
+      alert('이미 로그인 하셨습니다.');
+      location.href = '/keepcoding/main/index.php';
+    </script>";
+  
+}
+
+$userid = $_POST['userid'];
+  $userpw = $_POST['passwd'];
+  $passwd = hash('sha512',$userpw); //암호를 sha512 알고리즘이용 암호화
+  
+  $query = "select * from members where userid='{$userid}' and userpw='{$passwd}'"; 
+  $result = $mysqli->query($query);
+  $rs = $result->fetch_object();
+
+  if($rs){
+
+    $_SESSION['UID'] = $rs->userid;
+    $_SESSION['UNAME'] = $rs->username;
+
+    $sql = "UPDATE cart SET userid='{$userid}' where ssid='".session_id()."'";    
+    $result = $mysqli->query($sql);
+    echo "<script>
+      alert('$rs->username 님 반갑습니다');
+      location.href = '/keepcoding/main/index.php';
+    </script>";
+  } else{
+    echo "<script>
+      alert('아이디, 비번을 다시 확인하세요');
+      history.back();
+    </script>";
+  }
+
+include_once $_SERVER['DOCUMENT_ROOT'].'/keepcoding/main/inc/footer.php';
+?>
