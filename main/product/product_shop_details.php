ef66fd1ca56d9ef7e65a8596ba0925454cf0cd41
triallife <nanamutok@gmail.com>
2023-09-20 16:08:15 +0900

    [ADD] 강의 탐색, 상세보기 구현

--- main/product/product_shop_details.php
+++ main/product/product_shop_details.php
@@ -16,11 +16,6 @@
       foreach($rsc as $item){
         $type = $item->level;
         if($type != '숏강의'){ // 일반강의라면
-          if($item->price == 0){
-            $price = '무료 강의';
-          }else{
-            $price = '￦ '.$item->price;
-          }
 
           $cate = $item->cate;
           $cateNum = explode('/', $cate);
@@ -57,7 +52,9 @@
             <?= $bcn.'>'.$mcn.'>'.$smCate ?>
           </p>
           <p class="pshop_details_01_contetn"><?= $item-> product_intro ?></p>
-          <p class="h4 d-flex justify-content-end pshop_details_price"><span class="number"><?= $price ?></span></p>
+          <p class="h4 d-flex justify-content-end pshop_details_price gap-2">
+
+          </p>
         </div>
       </div>
 
@@ -68,7 +65,21 @@
         </svg>
         <p><?= $item -> sale_cnt ?></p>
       </div>
-      <button class="btn btn-lg btn-primary fs-6 h6">장바구니 담기</button>
+      <div class="d-flex justify-content-between p-0">
+        <p class="h4 pshop_details_02_price">
+          <?php
+            if($item->price == 0){
+              $price = '무료 강의';
+              $echo = $price;
+            }else{
+              $price = $item->price;
+              $echo = "<span>￦</span><span class=\"number\">$price</span>";
+            }
+            echo $echo;
+          ?>
+        </p>
+        <button class="btn btn-lg btn-primary fs-6 h6" data-pid="<?= $item->pid; ?>">장바구니 담기</button>
+      </div>
     </div>
 
     <div class="pshop_details_03">
@@ -87,21 +98,18 @@
           </div>
         </div>
         <div class="pshop_details_02 pshop_details_02_shorts d-flex justify-content-between">
-          <div class="d-flex">
+          <div class="d-flex gap-2">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 21C10.5 21 9 21 9 19.5C9 18 10.5 13.5 16.5 13.5C22.5 13.5 24 18 24 19.5C24 21 22.5 21 22.5 21H10.5ZM16.5 12C17.6935 12 18.8381 11.5259 19.682 10.682C20.5259 9.83807 21 8.69347 21 7.5C21 6.30653 20.5259 5.16193 19.682 4.31802C18.8381 3.47411 17.6935 3 16.5 3C15.3065 3 14.1619 3.47411 13.318 4.31802C12.4741 5.16193 12 6.30653 12 7.5C12 8.69347 12.4741 9.83807 13.318 10.682C14.1619 11.5259 15.3065 12 16.5 12ZM7.824 21C7.60163 20.5317 7.49074 20.0183 7.5 19.5C7.5 17.4675 8.52 15.375 10.404 13.92C9.46364 13.6303 8.48392 13.4886 7.5 13.5C1.5 13.5 0 18 0 19.5C0 21 1.5 21 1.5 21H7.824ZM6.75 12C7.74456 12 8.69839 11.6049 9.40165 10.9017C10.1049 10.1984 10.5 9.24456 10.5 8.25C10.5 7.25544 10.1049 6.30161 9.40165 5.59835C8.69839 4.89509 7.74456 4.5 6.75 4.5C5.75544 4.5 4.80161 4.89509 4.09835 5.59835C3.39509 6.30161 3 7.25544 3 8.25C3 9.24456 3.39509 10.1984 4.09835 10.9017C4.80161 11.6049 5.75544 12 6.75 12Z" fill="#212529"/>
             </svg>
             <p><?= $item -> sale_cnt ?></p>
           </div>
-          <button class="btn btn-lg btn-primary fs-6 h6 disabled">숏강의는 결제없이 바로 무료수강</button>
+          <button class="btn btn-primary fs-6 h6" onclick="location.href='/keepcoding/main/product/product_shop_list.php';">강의탐색으로 돌아가기</button>
+          <!-- <button class="btn btn-lg btn-primary fs-6 h6 disabled">숏강의는 결제없이 바로 무료수강</button> -->
         </div>
     <?php
         }}
     ?>
-
-    <div class="d-flex justify-content-end pshop_details_04 mt-5">
-      <button class="btn btn-primary fs-6 h6" onclick="location.href='/keepcoding/main/product/product_shop_list.php';">강의탐색으로 돌아가기</button>
-    </div>
   </section>
   <?php
   include_once $_SERVER['DOCUMENT_ROOT'].'/keepcoding/main/inc/footer.php';
--- main/product/product_shop_list.php
+++ main/product/product_shop_list.php
@@ -70,7 +70,7 @@
       
       <div class="pshop_section01_list">
         <form method="GET">
-          <div class="pshop_section01_category">
+          <div class="pshop_section01_category_wrap">
             <h3 class="h6">카테고리</h3>
             <div class="form-check">
               <input class="form-check-input" type="radio" name="category" id="category01" value="2">
@@ -146,6 +146,8 @@
         <?php
           if(isset($rsc)){
             foreach($rsc as $item){
+
+              // 중분류 카테고리명 추출
               $cate = $item->cate;
               $cateNum = explode('/', $cate);
               $middleNumber = $cateNum[1]; // 중간 숫자 추출
@@ -155,20 +157,43 @@
               while($cr = $cateresult -> fetch_object()){
                 $crs[] = $cr;
               }
-              foreach($crs as $cateName){$cateName2 = $cateName->name;}
+              foreach($crs as $cateName){$cateName2 = $cateName->name;};
+
+              // 제목 생략
+              // var_dump($item->name);
+              $name = $item->name;
+              // var_dump($name);
+              $maxLength = 25;
+
+              $length = mb_strlen($name, 'utf-8');
+              if($length <= $maxLength){
+                $name = $name;
+              }else{
+                $str = mb_substr($name, 0, $maxLength, 'utf-8');
+                $name = $str . '⋯';
+              }
+              // function truncateString($name, $maxLength) {
+              //   if (mb_strlen($name, 'utf-8') <= $maxLength) {
+              //       return $name; // 문자열의 길이가 제한 이하이면 그대로 반환
+              //   } else {
+              //       $truncatedStr = mb_substr($name, 0, $maxLength, 'utf-8'); // 제한 길이만큼 잘라냄
+              //       return $truncatedStr . '...'; // 말줄임표 추가
+              //   }
+              // }
+
+              // $newname = truncateString($name, $maxLength);
+              
         ?>
         <div class="card sec2 text-center p-0" data-bs-theme="dark">
-          <a href="">
-          <div class="card-img-top-wrap">
-            <a href="/keepcoding/main/product/product_shop_details.php?pid=<?= $item->pid ?>">
-              <img src="<?= $item->thumbnail ?>" class="card-img-top" alt="lecture img">
-            </a>
-          </div>
+          <a href="/keepcoding/main/product/product_shop_details.php?pid=<?= $item->pid ?>">
+            <div class="card-img-top-wrap">
+                <img src="<?= $item->thumbnail ?>" class="card-img-top" alt="lecture img">
+            </div>
           </a>
           <div class='card-body z-3'>
-              <p class='card-title text-center fw-semibold'><a href="/keepcoding/main/product/product_shop_details.php?pid=<?= $item->pid ?>"><?= $item->name ?></a></p>
-              <a href='${item.href02}' class='btn btn-primary fs-10 mt-2'><?= $cateName2 ?></a>
-              <a href='${item.href03}' class='btn btn-primary fs-10 mt-2'><?php
+              <p class='card-title text-center fw-semibold'><a href="/keepcoding/main/product/product_shop_details.php?pid=<?= $item->pid ?>"><?= $name ?></a></p>
+              <a href='' class='btn btn-primary fs-10 mt-2'><?= $cateName2 ?></a>
+              <a href='' class='btn btn-primary fs-10 mt-2'><?php
                   if($item->price == 0){
                     echo "무료 강의";
                   }else{
