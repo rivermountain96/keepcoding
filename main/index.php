034166febb46b8ea8a8d7b07c881f81780e8fc24
sunchoi22 <sunchoi22@pos.idserve.net>
2023-09-20 21:27:33 +0900

    [FIX] 사용하지 않는 링크 및 스크립트 삭제, a링크 올라감방지위해 "#"을 "#none"으로 변경

--- main/index.php
+++ main/index.php
@@ -258,4 +258,5 @@
 <?php
   include_once $_SERVER['DOCUMENT_ROOT'].'/keepcoding/main/inc/footer.php';
 ?>
-<script src="../main/js/test.js"></script>
+<script src="/keepcoding/main/js/test_data.js"></script>
+<script src="/keepcoding/main/js/test.js"></script>
