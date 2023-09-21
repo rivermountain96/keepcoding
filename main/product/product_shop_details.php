59e39d0af558290ce12e07fee013078f94a618d0
jeongewon <137126410+jeongewon@users.noreply.github.com>
2023-09-21 17:32:35 +0900

    [FIX] h1->h2 태그 수정

--- main/product/product_shop_details.php
+++ main/product/product_shop_details.php
@@ -85,7 +85,7 @@
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">장바구니 담기</h1>
+                <h2 class="modal-title fs-5" id="exampleModalLabel">장바구니 담기</h2>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body text-center">
