38fa575a566d08050e135a76f729b06276baae96
rivermountain96 <dlrkdtks7@gmail.com>
2023-09-21 16:06:33 +0900

    [FIX] myproduct_list.php 쿼리수정

--- main/mypage/myproduct_list.php
+++ main/mypage/myproduct_list.php
@@ -18,6 +18,10 @@
   } else {
     echo "쿼리 실행 오류: " . $mysqli->error;
   }
+
+  // products 테이블에서 사용자가 담은 강의 정보를 가져오는 쿼리
+  $sql_products = "SELECT * FROM products WHERE userid = '$userid'";
+  $result_products = $mysqli->query($sql_products);
 ?>
 
   <!-- myproduct_list 시작 -->
@@ -41,7 +45,14 @@
       </div>
 
       <div>
-
+        <!-- 사용자가 담은 강의 정보를 출력 -->
+        <?php
+          while ($row = $result_products->fetch_object()) {
+            $productName = $row->name;
+            $productCategory = $row->cate;
+            $productContent = $row->content;
+          // 필요한 강의 정보를 여기에 출력하는 코드 작성
+        ?>
         <div class="cart">
           <div class="cart_card shadow-sm mcbg-white w-100 d-flex justify-content-between">
             <div class="d-flex gap-4 row">
@@ -66,55 +77,9 @@
             </div>
           </div>
         </div>
-        <div class="cart">
-          <div class="cart_card shadow-sm mcbg-white w-100 d-flex justify-content-between">
-            <div class="d-flex gap-4 row">
-              <img src="../img/example10.png" alt="cart img" class="shadow-sm col">
-              <div class="cart_info d-flex flex-column justify-content-between col-8">
-                <div class="cart_about d-flex flex-column gap-1">
-                  <h3 class="h5"><a href="../product/product_shop_details.html" class="mc-gray1">HTML - 기본문법</a></h3><br>
-                  <p class="mc-gray4">프론트엔드>HTML>초급</p><br>
-                </div>
-                <p class="d-flex">본 수업은 HTML에 대한 심화된 내용을 다룹니다. 
-                  HTML의 기본문법과 HTML의 주요한 태그들에 대한 수업을 담고 있습니다.
-                  본 수업은 HTML에 대한 심화된 내용을 다룹니다. </p>
-              </div>
-            </div>
-            <div class="d-flex flex-column align-items-end col-1">
-              <a href="" class="mc-gray1 cart_trash justify-content-end">삭제
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="cart">
-          <div class="cart_card shadow-sm mcbg-white w-100 d-flex justify-content-between">
-            <div class="d-flex gap-4 row">
-              <img src="../img/example05.png" alt="cart img" class="shadow-sm col">
-              <div class="cart_info d-flex flex-column justify-content-between col-8">
-                <div class="cart_about d-flex flex-column gap-1">
-                  <h3 class="h5"><a href="../product/product_shop_details.html" class="mc-gray1">HTML - 기본문법</a></h3><br>
-                  <p class="mc-gray4">프론트엔드>HTML>초급</p><br>
-                </div>
-                <p class="d-flex">본 수업은 HTML에 대한 심화된 내용을 다룹니다. 
-                  HTML의 기본문법과 HTML의 주요한 태그들에 대한 수업을 담고 있습니다.
-                  본 수업은 HTML에 대한 심화된 내용을 다룹니다. </p>
-              </div>
-            </div>
-            <div class="d-flex flex-column align-items-end col-1">
-              <a href="" class="mc-gray1 cart_trash justify-content-end">삭제
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
-
+        <?php
+          }
+        ?>
       </div>
 
       <div class="d-flex justify-content-center">
@@ -129,7 +94,6 @@
         </nav>
       </div>
 
-    </div>
   </section>
   <!-- myproduct_list 끝 -->
 
