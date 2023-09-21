f1d0c7124792d723f53ae0eb0c61b64e3fece7ff
rivermountain96 <dlrkdtks7@gmail.com>
2023-09-21 17:16:12 +0900

    [FIX] myproduct_list.php 수정

--- main/mypage/myproduct_list.php
+++ main/mypage/myproduct_list.php
@@ -45,21 +45,15 @@
         <!-- 사용자가 담은 강의 정보를 출력 -->
         <?php
           // products 테이블에서 사용자가 담은 강의 정보를 가져오는 쿼리
-          $sql_products = "SELECT * FROM products WHERE 1 = 1";
+          $sql_products = "SELECT * FROM products WHERE 1=1";
           $result_products = $mysqli->query($sql_products);
 
           if ($result_products) {
-            $prs = $result_products->fetch_object();
-          } else {
-            echo "쿼리 실행 오류: " . $mysqli->error;
-          }
-
-          while ($prs = $result_products->fetch_object()) {
-            $productName = $prs->name;
-            $productCategory = $prs->cate;
-            $productContent = $prs->content;
-            $productIntro = $prs->product_intro;
-            // 필요한 강의 정보를 여기에 출력하는 코드 작성
+            while ($prs = $result_products->fetch_object()) {
+              $productName = $prs->name;
+              $productCategory = $prs->cate;
+              $productContent = $prs->content;
+              $productIntro = $prs->product_intro;
         ?>
         <div class="cart">
           <div class="cart_card shadow-sm mcbg-white w-100 d-flex justify-content-between">
@@ -85,6 +79,7 @@
         </div>
         <?php
             }
+          } 
         ?>
       </div>
 
